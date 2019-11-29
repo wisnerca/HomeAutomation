2d44d043d321dccf0ca376afa6e246564e7ff570
Christian Wisner-Carlson <c@fofk.org>
2019-11-29 13:34:49 -0800

    update directory sstructure

---
+++ rpi_speedcontrol/speedthreaded1.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python
+import sys
+import Adafruit_DHT
+import time
+import RPi.GPIO as GPIO
+import threading
+GPIO.setmode(GPIO.BCM)
+LED = 15
+GPIO.setup(LED,GPIO.OUT)
+switchtemp = 77
+hyst = 1.5
+timewait = 10
+state = True
+state1 = True
+
+def doStuff():
+    laststate = False
+    while True:
+        if (state1):
+            while (state1):
+                    GPIO.output(LED, True)
+                    time.sleep(ratio * timestep)
+                    GPIO.output(LED, False)
+                    time.sleep((1-ratio) * timestep)
+            laststate = True
+        else: 
+                GPIO.output(LED, False)
+                laststate = False
+
+ratio = 0.35
+timestep = 0.1
+GPIO.output(LED, False)
+t = threading.Thread(target=doStuff)
+t.start()
+
+
+while True:
+    humidity, temperature = Adafruit_DHT.read_retry(11, 4)
+    fahrenheit = (temperature * 1.8) + 32
+    print 'Temp: {0:0.1f} F  Humidity: {1:0.1f} %'.format(fahrenheit, humidity)
+    oldstate = state
+    if (state):
+        if (fahrenheit <= switchtemp + hyst/2):
+            state = False
+        else:
+            state = True
+    else:
+        if (fahrenheit <= switchtemp - hyst/2):
+            state = False
+        else:
+            state = True
+    if (state != oldstate):
+        print "waiting " + str(timewait) + " seconds"
+        time.sleep(timewait)
+    else:
+        state1 = state
+    print state
+
+
