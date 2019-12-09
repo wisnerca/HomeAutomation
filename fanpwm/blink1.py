d8cd28b730c8a500cbd217284121ac9ba3098f82
Christian Wisner-Carlson <c@fofk.org>
2019-12-09 06:52:47 +0000

    add pwm stuff for dc

---
+++ fanpwm/blink1.py
@@ -0,0 +1,18 @@
+import RPi.GPIO as GPIO
+import time
+import sys
+ratio = 0.75
+timestep = 0.1
+GPIO.setmode(GPIO.BCM)
+LED = 27
+ledState = True
+GPIO.setup(LED,GPIO.OUT)
+ratio = float(sys.argv[1])
+print ratio
+while True:
+#    ledState = not ledState
+    GPIO.output(LED, True)
+    time.sleep(ratio * timestep)
+    GPIO.output(LED, False)
+    time.sleep((1-ratio) * timestep)
+
